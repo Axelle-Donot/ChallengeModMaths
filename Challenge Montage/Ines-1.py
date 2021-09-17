836eccfeb5471d84c2f5b45dc3c237aed93b418c
ines.garcia <inesgarcia861@gmail.com>
2021-09-17 11:14:06 +0200

    oui

--- Challenge Montage/Ines-1.py
+++ Challenge Montage/Ines-1.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-"""                          COTE A COTE
+                         #COTE A COTE
 usb = plt.imread('usb.png','PNG')
 desktop = plt.imread('desktop.png','PNG')
 
@@ -14,30 +14,4 @@ desktoptab = desktop
 final = np.concatenate((usbtab,desktoptab), axis =1) #on concatene les tableaux
 plt.imshow(final) #on affiche le tout
 plt.show()
-"""
-
-
-"""                   COTE A COTE ET AU DESSUS
-usb1 = plt.imread('usb.png','PNG')
-usb2 = usb1
-
-desktop = plt.imread('desktop.png','PNG')
-
-desktoptab = 255 * np.ones((600, 600,3), dtype = np.uint8)
-desktoptab = desktop
-usbtab1 = 255 * np.ones((600, 300,3), dtype = np.uint8)
-
-usbtab2 = usbtab1 #création plus rapide, clonage de tableau
-
-usbtab1 = usb1
-usbtab2 = usb2
-
-usbcolle = np.concatenate((usbtab1,usbtab2), axis =1)
-
-final = np.concatenate((usbcolle,desktoptab), axis =0)
-plt.imshow(final)
-plt.show
-"""
-
-
 
