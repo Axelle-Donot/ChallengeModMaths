b0056d5eefb854de34ab7d62e1d808cefa4b9c06
Axelle Donot <axelle.donot@etu.umontpellier.fr>
2021-09-17 11:10:59 +0200

    Modification de l'image 6

--- Challenge Montage/Axelle-6.py
+++ Challenge Montage/Axelle-6.py
@@ -6,14 +6,8 @@ imgordi = plt.imread("desktop.png", "PNG")
 
 imgUsb1 = imgUsb.transpose (1,0,2)
 imgordi1 = imgordi.transpose (1,0,2)
-imgordi2 = imgordi1 [:,::-1]
 
-
-print (imgUsb.shape)
-print (imgUsb1.shape)
-
-imgfinal = np.concatenate ((imgUsb1,imgordi2), axis=0)
-
+imgfinal = np.concatenate ((imgUsb1,imgordi1[:,::-1]), axis=0)
 
 plt.imshow(imgfinal)
 plt.show()
