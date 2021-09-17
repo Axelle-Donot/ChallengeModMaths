b110090b9e10ac6a84d4ddbe053b0fa7c06be88c
alexi-mathioupollak <alexi.mathiou-pollak@etu.umontpellier.fr>
2021-09-17 10:36:16 +0200

    montage 2 et 5 finished

--- montage5.py
+++ montage5.py
@@ -2,9 +2,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Création des variables des images
 usb = plt.imread('usb.png', 'PNG')
 desktop = plt.imread('desktop.png', 'PNG')
+
+# Rotation de l'image desktop.png
 desktopTrans = desktop.transpose((1, 0, 2))
+
 print(desktopTrans.shape)
+
+# Ajout des 2 images sur le graphique
 double = np.concatenate((usb, desktopTrans), axis=1)
+
+# Affichage des deux images
 plt.imshow(double)
