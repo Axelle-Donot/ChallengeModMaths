5b22d283d19fff20e135707b503dad0e01b659fd
alexi-mathioupollak <alexi.mathiou-pollak@etu.umontpellier.fr>
2021-09-18 18:40:39 +0200

    Seychelles drapeau terminé

---
+++ Challenge Droites/Alexi - Seychelles.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 18 18:11:47 2021
+
+@author: alexi
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# On définit les couleurs avant tout
+bleu = np.array([0, 0, 135], dtype = np.uint8)
+rouge = np.array([255, 0, 0], dtype = np.uint8)
+vert = np.array([5, 119, 0], dtype = np.uint8)
+jaune = np.array([255, 216, 0], dtype = np.uint8)
+
+# On définit la taille de l'image 
+nc, nl= (300, 180)
+
+# On initialise l'image blanche avec les coordonnées rentrées
+flag = np.ones((nl, nc, 3), dtype = np.uint8) * 255
+
+# On crée les coordonnées avec une origine au centre de l'image
+X, Y = np.meshgrid(np.linspace(0, nc, nc),\
+                   np.linspace(-nl, 0, nl), \
+                   indexing = 'xy')
+    
+ibleu = (Y <= (3/2) * X) & (Y <= - (3/2) * X)
+ijaune = (Y <= 1) & (Y >= - (3/2) * X) & (Y <= - (15 / 18) * X)
+irouge = (Y >= - (15 / 18) * X) & (Y <= - (2 / 5) * X)
+ivert = (Y >= - (3 / 14) * X)
+
+flag[ibleu,:] = bleu
+flag[ijaune,:] = jaune
+flag[irouge,:] = rouge
+flag[ivert,:] = vert
+
+plt.imshow(flag)
