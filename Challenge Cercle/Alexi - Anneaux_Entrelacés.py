5175732c7d3f5448cef81ff7cbb2552ba9b4a5e3
alexi-mathioupollak <alexi.mathiou-pollak@etu.umontpellier.fr>
2021-09-24 10:18:29 +0200

    Update Alexi - Anneaux_Entrelacés.py

--- Challenge Cercle/Alexi - Anneaux_Entrelacés.py
+++ Challenge Cercle/Alexi - Anneaux_Entrelacés.py
@@ -6,8 +6,25 @@ Created on Fri Sep 24 10:04:25 2021
 """
 
 import numpy as np
-import matplotlib.pyplot = plt 
+import matplotlib.pyplot as plt 
 
-bleu = np.array[(0, 0, 255), dtype = np.uint8]
-vert = np.array[()]
-orange
+bleu = np.array([0, 0, 255], dtype = np.uint8)
+vert = np.array([22, 184, 78], dtype = np.uint8)
+orange = np.array([255, 127, 14], dtype = np.uint8)
+
+nc, nl = (400, 300)
+
+flag = np.ones((nl, nc, 3), dtype = np.uint8) * 255 
+
+X, Y = np.meshgrid(np.linspace(-nc/2 + 0.5, nc/2 - 0.5, nc), \
+                   np.linspace(nl/2 - 0.5, -nl/2 + 0.5, nl), \
+                    indexing = 'xy')
+                            
+r = 0.4 * nl
+r2 = 0.35 * nl
+
+flag[:,:] = bleu
+flag[(X*X + Y*Y) <= (r*r), :] = vert
+flag[(X*X + Y*Y) <= (r2*r2), :] = bleu
+
+plt.imshow(flag)
